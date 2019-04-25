2b7d85000a4f4df92d379e2e8c81819f2a035336
Adrian Kunz <clashsoft@hotmail.com>
2019-04-25 11:08:58 +0200

    Add Config class
    #17

---
+++ src/main/java/org/fulib/scenarios/config/Config.java
@@ -0,0 +1,68 @@
+package org.fulib.scenarios.config;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config
+{
+   // =============== Fields ===============
+
+   private String       modelDir;
+   private String       testDir;
+   private List<String> inputDirs = new ArrayList<>();
+
+   // =============== Properties ===============
+
+   public String getModelDir()
+   {
+      return this.modelDir;
+   }
+
+   public void setModelDir(String modelDir)
+   {
+      this.modelDir = modelDir;
+   }
+
+   public String getTestDir()
+   {
+      return this.testDir;
+   }
+
+   public void setTestDir(String testDir)
+   {
+      this.testDir = testDir;
+   }
+
+   public List<String> getInputDirs()
+   {
+      return this.inputDirs;
+   }
+
+   // =============== Methods ===============
+
+   public Options createOptions()
+   {
+      final Options options = new Options();
+
+      final Option modelDir = new Option("m", "modelDir", true, "model output directory, default: src/main/java");
+      modelDir.setRequired(false);
+      options.addOption(modelDir);
+
+      final Option testDir = new Option("t", "testDir", true, "test output directory, default: src/test/java");
+      testDir.setRequired(false);
+      options.addOption(testDir);
+
+      return options;
+   }
+
+   public void readOptions(CommandLine cmd)
+   {
+      this.setModelDir(cmd.getOptionValue("modelDir", "src/main/java"));
+      this.setTestDir(cmd.getOptionValue("testDir", "src/test/java"));
+      this.getInputDirs().addAll(cmd.getArgList());
+   }
+}
