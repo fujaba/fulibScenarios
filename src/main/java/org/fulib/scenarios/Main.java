7848b6bee16eeddbdd34a683e465f65dabe31bd4
Adrian Kunz <clashsoft@hotmail.com>
2019-04-25 11:29:44 +0200

    Main class
    Closes #17

---
+++ src/main/java/org/fulib/scenarios/Main.java
@@ -0,0 +1,9 @@
+package org.fulib.scenarios;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      new ScenarioCompiler().run(System.in, System.out, System.err, args);
+   }
+}
