d2076ebd4f51861d7a84c85caf0c0edbd4ed1fc5
Adrian Kunz <clashsoft@hotmail.com>
2019-04-28 23:05:21 +0200

    Use ModelManager for CodeGenerator

--- src/main/java/org/fulib/scenarios/transform/CodeGenerator.java
+++ src/main/java/org/fulib/scenarios/transform/CodeGenerator.java
@@ -1,12 +1,24 @@
 package org.fulib.scenarios.transform;
 
 import org.fulib.Generator;
+import org.fulib.StrUtil;
 import org.fulib.builder.ClassBuilder;
 import org.fulib.builder.ClassModelBuilder;
+import org.fulib.builder.ClassModelManager;
+import org.fulib.classmodel.Clazz;
 import org.fulib.classmodel.FMethod;
+import org.fulib.scenarios.ast.NamedExpr;
 import org.fulib.scenarios.ast.Scenario;
 import org.fulib.scenarios.ast.ScenarioGroup;
 import org.fulib.scenarios.ast.decl.VarDecl;
+import org.fulib.scenarios.ast.expr.Expr;
+import org.fulib.scenarios.ast.expr.access.AttributeAccess;
+import org.fulib.scenarios.ast.expr.access.ExampleAccess;
+import org.fulib.scenarios.ast.expr.call.CreationExpr;
+import org.fulib.scenarios.ast.expr.primary.NameAccess;
+import org.fulib.scenarios.ast.expr.primary.NumberLiteral;
+import org.fulib.scenarios.ast.expr.primary.PrimaryExpr;
+import org.fulib.scenarios.ast.expr.primary.StringLiteral;
 import org.fulib.scenarios.ast.sentence.Sentence;
 import org.fulib.scenarios.ast.sentence.ThereSentence;
 import org.fulib.scenarios.tool.Config;
@@ -15,7 +27,7 @@ public class CodeGenerator implements ScenarioGroup.Visitor<Object, Object>, Sce
 {
    private final Config config;
 
-   private ClassModelBuilder modelBuilder;
+   private ClassModelManager modelManager;
    private ClassModelBuilder testBuilder;
 
    private ClassBuilder classBuilder;
@@ -32,7 +44,8 @@ public class CodeGenerator implements ScenarioGroup.Visitor<Object, Object>, Sce
    @Override
    public Object visit(ScenarioGroup scenarioGroup, Object par)
    {
-      this.modelBuilder = new ClassModelBuilder(scenarioGroup.getName(), this.config.getModelDir());
+      this.modelManager = new ClassModelManager().havePackageName(scenarioGroup.getName())
+                                                 .haveMainJavaDir(this.config.getModelDir());
       this.testBuilder = new ClassModelBuilder(scenarioGroup.getName(), this.config.getTestDir());
 
       for (final Scenario scenario : scenarioGroup.getScenarios())
@@ -40,7 +53,7 @@ public class CodeGenerator implements ScenarioGroup.Visitor<Object, Object>, Sce
          scenario.accept(this, par);
       }
 
-      new Generator().generate(this.modelBuilder.getClassModel());
+      new Generator().generate(this.modelManager.getClassModel());
       new Generator().generate(this.testBuilder.getClassModel());
       return null;
    }
@@ -60,7 +73,7 @@ public class CodeGenerator implements ScenarioGroup.Visitor<Object, Object>, Sce
       }
 
       final FMethod testMethod = new FMethod().writeName("test").writeReturnType("void")
-                                     .setMethodBody(this.bodyBuilder.toString());
+                                              .setMethodBody(this.bodyBuilder.toString());
       this.classBuilder.getClazz().withMethods(testMethod);
 
       return null;
