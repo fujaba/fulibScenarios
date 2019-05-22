3a6606902a34c3735566f4c42104728d883cfac2
Adrian Kunz <clashsoft@hotmail.com>
2019-05-22 21:32:08 +0200

    Add JavaCompiler.genCompileRun
    (*) Made junit a main dependency.
    (+) Added the JavaCompiler.scenarioc and JavaCompiler.genCompileRun methods.

--- src/main/java/org/fulib/scenarios/tool/JavaCompiler.java
+++ src/main/java/org/fulib/scenarios/tool/JavaCompiler.java
@@ -2,22 +2,30 @@ package org.fulib.scenarios.tool;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import javax.tools.ToolProvider;
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class JavaCompiler
 {
    public static int javac(String classpath, Path outFolder, Path... sourceFolders) throws IOException
+   {
+      return javac(null, null, classpath, outFolder, sourceFolders);
+   }
+
+   public static int javac(OutputStream out, OutputStream err, String classpath, Path outFolder, Path... sourceFolders)
+      throws IOException
    {
       Files.createDirectories(outFolder);
 
@@ -33,7 +41,7 @@ public class JavaCompiler
          args.add(classpath);
       }
 
-      return ToolProvider.getSystemJavaCompiler().run(null, System.out, System.err, args.toArray(new String[0]));
+      return ToolProvider.getSystemJavaCompiler().run(null, out, err, args.toArray(new String[0]));
    }
 
    public static boolean isJava(Path file)
@@ -104,6 +112,94 @@ public class JavaCompiler
       catch (IOException ex)
       {
          throw new RuntimeException("failed to walk " + testDir.toString(), ex);
+      }
+   }
+
+   public static int scenarioc(OutputStream out, OutputStream err, Path srcDir, Path modelFolder, Path testsFolder,
+      String... args)
+   {
+      final List<String> finalArgs = new ArrayList<>(5 + args.length);
+      finalArgs.add("-m");
+      finalArgs.add(modelFolder.toString());
+      finalArgs.add("-t");
+      finalArgs.add(testsFolder.toString());
+      finalArgs.add(srcDir.toString());
+      Collections.addAll(finalArgs, args);
+      return new ScenarioCompiler().run(null, out, err, finalArgs.toArray(new String[0]));
+   }
+
+   public static int genCompileRun(//
+      OutputStream out, OutputStream err,//
+      Path srcDir, //
+      Path modelFolder, Path testsFolder,//
+      Path modelOutFolder, Path testOutFolder,//
+      String... scenariocArgs) throws Exception
+   {
+      // prevent dock icon on Mac
+      try (FakeProperty ignored = new FakeProperty("apply.awt.UIElement", "true"))
+      {
+         final int scenarioc = scenarioc(out, err, srcDir, modelFolder, testsFolder, scenariocArgs);
+         if (scenarioc != 0)
+         {
+            return -1;
+         }
+
+         String classPath = System.getProperty("java.class.path");
+
+         final int modelJavac = javac(out, err, classPath, modelOutFolder, modelFolder);
+         if (modelJavac != 0)
+         {
+            return -2;
+         }
+
+         final String testClassPath = modelOutFolder + File.pathSeparator + classPath;
+         final int testJavac = javac(out, err, testClassPath, testOutFolder, testsFolder);
+         if (testJavac != 0)
+         {
+            return -3;
+         }
+
+         // call all test methods
+         final Result testResult = JavaCompiler.runTests(modelOutFolder, testOutFolder);
+
+         for (final Failure failure : testResult.getFailures())
+         {
+            final PrintStream stream = new PrintStream(err, false, StandardCharsets.UTF_8.name());
+            stream.print(failure.getTestHeader());
+            stream.println("failed:");
+
+            failure.getException().printStackTrace(stream);
+            stream.flush();
+         }
+
+         return testResult.getFailureCount();
+      }
+   }
+
+   // TODO move to Dyvil library
+   private static class FakeProperty implements AutoCloseable
+   {
+      private final String key;
+      private final String original;
+
+      public FakeProperty(String key, String newValue)
+      {
+         this.key = key;
+         this.original = System.getProperty(key);
+         System.setProperty(key, newValue);
+      }
+
+      @Override
+      public void close()
+      {
+         if (this.original == null)
+         {
+            System.clearProperty(this.key);
+         }
+         else
+         {
+            System.setProperty(this.key, this.original);
+         }
       }
    }
 }
--- src/test/java/org/fulib/scenarios/TestCodeGen.java
+++ src/test/java/org/fulib/scenarios/TestCodeGen.java
@@ -2,15 +2,11 @@ package org.fulib.scenarios;
 
 import org.fulib.scenarios.tool.JavaCompiler;
 import org.junit.Test;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 //@Ignore
 public class TestCodeGen
@@ -18,31 +14,14 @@ public class TestCodeGen
    @Test
    public void testGenCompileRun() throws Exception
    {
+      final Path srcFolder = Paths.get("src", "main", "scenarios");
       final Path testOutFolder = Paths.get("temp", "out", "test");
       final Path modelOutFolder = Paths.get("temp", "out", "model");
       final Path testsFolder = Paths.get("temp", "tests");
       final Path modelFolder = Paths.get("temp", "model");
 
-      Main.main(new String[] { "-m", "temp/model/", "-t", "temp/tests/", "src/test/scenarios/" });
-
-      String classPath = System.getProperty("java.class.path");
-
-      int returnCode = JavaCompiler.javac(classPath, modelOutFolder, modelFolder);
-      assertThat(returnCode, equalTo(0));
-
-      final String testClassPath = modelOutFolder + File.pathSeparator + classPath;
-      int returnCode2 = JavaCompiler.javac(testClassPath, testOutFolder, testsFolder);
-      assertThat(returnCode2, equalTo(0));
-
-      // call all test methods
-      final Result testResult = JavaCompiler.runTests(modelOutFolder, testOutFolder);
-
-      for (final Failure failure : testResult.getFailures())
-      {
-         System.err.println(failure.getTestHeader() + " failed:");
-         failure.getException().printStackTrace();
-      }
-
-      assertThat(testResult.getFailureCount(), equalTo(0));
+      assertEquals(0, JavaCompiler
+                         .genCompileRun(System.out, System.err, srcFolder, modelFolder, testsFolder, modelOutFolder,
+                                        testOutFolder));
    }
 }
