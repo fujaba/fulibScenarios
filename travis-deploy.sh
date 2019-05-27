8b677c9b1c699a31c4f4ce1ca42c83358c413fc5
Sebastian Copei <seb@uni-kassel.de>
2019-05-27 16:01:37 +0200

    add awesome deploy script

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_d171de1d648a_key -iv $encrypted_d171de1d648a_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
-fi
-if [ "$TRAVIS_BRANCH" = 'release' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K $encrypted_d171de1d648a_key -iv $encrypted_d171de1d648a_iv -in my.travis.gpg.enc -out my.travis.gpg -d
-  ./gradlew release -Prelease.useAutomaticVersion=true  -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=my.travis.gpg
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t "$DOCKER_USERNAME"/fulib_scenarios:latest .
+docker push "$DOCKER_USERNAME"/fulib_scenarios:latest
 
+# update on rancher
+rancherUrl="http://avocado.uniks.de:8080/v2-beta/projects/1a5/services/1s173"
+curl -u "$RANCHER_ACCESS":$"RANCHER_KEY" \
+-X POST \
+-H 'Content-Type: application/json' \
+-d '{"inServiceStrategy": { "startFirst": true }}' \
+${rancherUrl}?action=upgrade
+
+echo "Waiting for upgrade ..."
+retry=60
+state=$(curl -u "$RANCHER_ACCESS":$"RANCHER_KEY" ${rancherUrl} | jq -r '.state')
+while [[ "$state" !=  "upgraded"  ]] && [[ "$retry" >= 0 ]]
+    do
+        state=$(curl -u "$RANCHER_ACCESS":$"RANCHER_KEY" ${rancherUrl} | jq -r '.state')
+        retry=${retry}-1
+        sleep 1
+done
+
+echo "Finishing upgrade ..."
+curl - u "$RANCHER_ACCESS":$"RANCHER_KEY" ${rancherUrl}?action=finishupgrade
+
+retry=60
+state=$(curl -u "$RANCHER_ACCESS":$"RANCHER_KEY" ${rancherUrl} | jq -r '.state')
+while [[ "$state" !=  "active"  ]] && [[ "$retry" >= 0 ]]
+    do
+        state=$(curl -u "$RANCHER_ACCESS":$"RANCHER_KEY" ${rancherUrl} | jq -r '.state')
+        retry=${retry}-1
+        sleep 1
+done
+
+if [[ "$retry" > 0 ]]
+    then
+        echo "Successfully upgraded!"
+    else
+        echo "Error while upgrading, please check on rancher!"
+fi
