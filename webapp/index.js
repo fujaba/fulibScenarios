3b67fd172420deb035a7542c7cf99c838e0a5f18
Adrian Kunz <clashsoft@hotmail.com>
2019-06-08 23:00:04 +0200

    Clean up Web App html and js

--- webapp/index.js
+++ webapp/index.js
@@ -1,70 +1,70 @@
 // =============== Constants ===============
 
-const apiUrl = "";
+const apiUrl = '';
 
 const examples = [
-	'Definitions', [
-		'Simple Definitions',
-		'Complex Definitions',
-	],
-	'Testing', [
-		'Expectations',
-		'Diagrams',
-	],
-	'Methods', [
-		'Calling',
-	],
+    'Definitions', [
+        'Simple Definitions',
+        'Complex Definitions',
+    ],
+    'Testing', [
+        'Expectations',
+        'Diagrams',
+    ],
+    'Methods', [
+        'Calling',
+    ],
 ];
 
 const exampleSelect = document.getElementById('exampleSelect');
-var selectedExample = "";
+let selectedExample = '';
 
-const scenarioInput = document.getElementById("scenarioInput");
+const scenarioInput = document.getElementById('scenarioInput');
 const scenarioInputCodeMirror = CodeMirror.fromTextArea(scenarioInput, {
-	mode: 'markdown',
-	lineNumbers: true,
-	lineWrapping: true,
-	styleActiveLine: true,
-	extraKeys: {
-		'Ctrl-Enter': submit,
-		'Cmd-Enter': submit
-	}
+    mode: 'markdown',
+    lineNumbers: true,
+    lineWrapping: true,
+    styleActiveLine: true,
+    extraKeys: {
+        'Ctrl-Enter': submit,
+        'Cmd-Enter': submit,
+    },
 });
 
-const javaTestOutput = document.getElementById("javaTestOutput");
+const javaTestOutput = document.getElementById('javaTestOutput');
 const javaTestOutputCodeMirror = CodeMirror.fromTextArea(javaTestOutput, {
-	lineNumbers: true,
-	matchBrackets: true,
-	readOnly: true,
-	mode: 'text/x-java'
+    lineNumbers: true,
+    matchBrackets: true,
+    readOnly: true,
+    mode: 'text/x-java',
 });
 
 // =============== Initialization ===============
 
-init()
+init();
 
 function init() {
-	for (let i = 0; i < examples.length; i += 2) {
-		const groupName = examples[i];
-		const groupItems = examples[i + 1];
+    for (let i = 0; i < examples.length; i += 2) {
+        const groupName = examples[i];
+        const groupItems = examples[i + 1];
 
-		const optgroup = document.createElement("optgroup");
-		optgroup.label = groupName;
+        const optgroup = document.createElement('optgroup');
+        optgroup.label = groupName;
 
-		for (let groupItem of groupItems) {
-			const option = document.createElement("option");
+        for (let groupItem of groupItems) {
+            const option = document.createElement('option');
 
-			option.value = groupName.toLowerCase() + "/" + groupItem.replace(" ", "");
-			option.label = groupItem;
-			option.innerText = groupItem;
+            option.value = groupName.toLowerCase() + '/' + groupItem.replace(' ', '');
+            option.label = groupItem;
+            option.innerText = groupItem;
 
-			optgroup.appendChild(option);
-		}
+            optgroup.appendChild(option);
+        }
 
-		exampleSelect.appendChild(optgroup);
-	}
+        exampleSelect.appendChild(optgroup);
+    }
 
-	selectExample("")
+    selectExample('');
 }
 
 // =============== Functions ===============
@@ -72,92 +72,90 @@ function init() {
 // --------------- Event Handlers ---------------
 
 function submit() {
-	const text = scenarioInputCodeMirror.getValue();
+    const text = scenarioInputCodeMirror.getValue();
 
-	if ( ! selectedExample) {
-	    localStorage.setItem('fulibScenario', text);
-	}
+    if (!selectedExample) {
+        localStorage.setItem('fulibScenario', text);
+    }
 
-	javaTestOutputCodeMirror.setValue("// loading...");
+    javaTestOutputCodeMirror.setValue('// loading...');
 
-	api("POST", apiUrl + "/runcodegen", {
-		scenarioText: text
-	}, function(response) {
-		console.log(response.output)
-		console.log("exit code: " + response.exitCode)
+    api('POST', apiUrl + '/runcodegen', {
+        scenarioText: text,
+    }, function(response) {
+        console.log(response.output);
+        console.log('exit code: ' + response.exitCode);
 
-		let javaCode = '';
-		if (response.exitCode != 0) {
-			javaCode += response.output.split("\n").map(function(line) {
-            				return "// " + line;
-            			}).join("\n") + '\n';
-		}
-		if (response.testMethods) {
-			for (testMethod of response.testMethods) {
-				javaCode += "// " + testMethod.name + '\n';
-				javaCode += testMethod.body;
-			}
-		}
-		javaTestOutputCodeMirror.setValue(javaCode);
+        let javaCode = '';
+        if (response.exitCode !== 0) {
+            javaCode += response.output.split('\n').map(function(line) {
+                return '// ' + line;
+            }).join('\n') + '\n';
+        }
+        if (response.testMethods) {
+            for (testMethod of response.testMethods) {
+                javaCode += '// ' + testMethod.name + '\n';
+                javaCode += testMethod.body;
+            }
+        }
+        javaTestOutputCodeMirror.setValue(javaCode);
 
-		document.getElementById("objectDiagram").innerHTML = response.objectDiagram;
-		document.getElementById("classDiagram").innerHTML = response.classDiagram;
+        document.getElementById('objectDiagram').innerHTML = response.objectDiagram;
+        document.getElementById('classDiagram').innerHTML = response.classDiagram;
 
-		// TODO display errors
-	});
+        // TODO display errors
+    });
 }
 
 function selectExample(value) {
     selectedExample = value;
 
-	if (!value) {
-	    const oldValue = localStorage.getItem('fulibScenario');
+    if (!value) {
+        const oldValue = localStorage.getItem('fulibScenario');
 
-	    if (oldValue) {
-	        scenarioInputCodeMirror.setValue(oldValue);
-	        submit();
-	    }
-	    else {
-		    scenarioInputCodeMirror.setValue(""
-		        + "// start typing your scenario or select an example using the dropdown above.\n\n"
-		        + "# Scenario My First. \n\n"
-                + "There is a Car with name Herbie. \n");
-	    }
-		return;
-	}
+        if (oldValue) {
+            scenarioInputCodeMirror.setValue(oldValue);
+            submit();
+        } else {
+            scenarioInputCodeMirror.setValue(''
+                + '// start typing your scenario or select an example using the dropdown above.\n\n'
+                + '# Scenario My First. \n\n'
+                + 'There is a Car with name Herbie. \n');
+        }
+        return;
+    }
 
-	const url = "examples/" + value + ".md";
-	const request = new XMLHttpRequest();
+    const url = 'examples/' + value + '.md';
+    const request = new XMLHttpRequest();
 
-	scenarioInputCodeMirror.setValue("// loading...");
+    scenarioInputCodeMirror.setValue('// loading...');
 
-    request.addEventListener("load", function () {
-    	if (this.status == 200) {
-    		const text = this.responseText;
-        	scenarioInputCodeMirror.setValue(text);
+    request.addEventListener('load', function() {
+        if (this.status === 200) {
+            const text = this.responseText;
+            scenarioInputCodeMirror.setValue(text);
 
-        	// auto run code gen
-        	submit();
-    	}
-    	else {
-    		scenarioInputCodeMirror.setValue("// failed to load " + url + ": " + this.status);
-    	}
+            // auto run code gen
+            submit();
+        } else {
+            scenarioInputCodeMirror.setValue('// failed to load ' + url + ': ' + this.status);
+        }
     });
-    request.open("GET", url, true);
+    request.open('GET', url, true);
     request.send();
 }
 
 // --------------- Helpers ---------------
 
 function api(method, url, body, handler) {
-	const requestBody = JSON.stringify(body);
+    const requestBody = JSON.stringify(body);
     const request = new XMLHttpRequest();
 
-    request.overrideMimeType("application/json");
-    request.addEventListener("load", function () {
-        handler(JSON.parse(this.responseText))
+    request.overrideMimeType('application/json');
+    request.addEventListener('load', function() {
+        handler(JSON.parse(this.responseText));
     });
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     request.send(requestBody);
 }
