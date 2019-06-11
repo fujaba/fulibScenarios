84b443c5140c10b908ce76a821c84f813fef7fc2
Adrian Kunz <clashsoft@hotmail.com>
2019-06-11 13:33:01 +0200

    Add TypeGenerator
    #27

---
+++ src/main/java/org/fulib/scenarios/codegen/TypeGenerator.java
@@ -0,0 +1,39 @@
+package org.fulib.scenarios.codegen;
+
+import org.fulib.scenarios.ast.type.*;
+
+public enum TypeGenerator implements Type.Visitor<CodeGenerator, String>
+{
+   INSTANCE;
+
+   @Override
+   public String visit(Type type, CodeGenerator par)
+   {
+      throw new UnsupportedOperationException();
+   }
+
+   @Override
+   public String visit(UnresolvedType unresolvedType, CodeGenerator par)
+   {
+      throw new UnsupportedOperationException("unresolved type " + unresolvedType.getName());
+   }
+
+   @Override
+   public String visit(ClassType classType, CodeGenerator par)
+   {
+      return classType.getClassDecl().getName();
+   }
+
+   @Override
+   public String visit(ListType listType, CodeGenerator par)
+   {
+      par.addImport("java.util.List");
+      return "List<" + listType.getElementType().accept(this, par) + ">";
+   }
+
+   @Override
+   public String visit(PrimitiveType primitiveType, CodeGenerator par)
+   {
+      return primitiveType.getJavaName();
+   }
+}
