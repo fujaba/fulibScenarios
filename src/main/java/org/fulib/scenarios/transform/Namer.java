c56ee7d720934aaa53e43b15f485ddfaded63949
Adrian Kunz <clashsoft@hotmail.com>
2019-06-11 14:00:22 +0200

    Namer implements Type.Visitor

--- src/main/java/org/fulib/scenarios/transform/Namer.java
+++ src/main/java/org/fulib/scenarios/transform/Namer.java
@@ -19,12 +19,45 @@ import org.fulib.scenarios.ast.expr.primary.PrimaryExpr;
 import org.fulib.scenarios.ast.expr.primary.StringLiteral;
 import org.fulib.scenarios.ast.sentence.AnswerSentence;
 import org.fulib.scenarios.ast.sentence.Sentence;
+import org.fulib.scenarios.ast.type.*;
 
 import java.util.List;
 
-public enum Namer implements Expr.Visitor<Object, String>, Name.Visitor<Object, String>
+public enum Namer implements Type.Visitor<Object, String>, Expr.Visitor<Object, String>, Name.Visitor<Object, String>
 {
    INSTANCE;
+
+   // --------------- Type.Visitor ---------------
+
+   @Override
+   public String visit(Type type, Object par)
+   {
+      throw new UnsupportedOperationException();
+   }
+
+   @Override
+   public String visit(UnresolvedType unresolvedType, Object par)
+   {
+      return unresolvedType.getName();
+   }
+
+   @Override
+   public String visit(ClassType classType, Object par)
+   {
+      return classType.getClassDecl().getName();
+   }
+
+   @Override
+   public String visit(ListType listType, Object par)
+   {
+      return "List";
+   }
+
+   @Override
+   public String visit(PrimitiveType primitiveType, Object par)
+   {
+      return primitiveType.getJavaName();
+   }
 
    // --------------- Expr.Visitor ---------------
 
