db2fd02a8ebf0e99212aa7bdddd79f7b3ad830ff
Adrian Kunz <clashsoft@hotmail.com>
2019-06-27 00:32:30 +0200

    [experimental] ErrorListener reports parser ambiguities

--- src/main/java/org/fulib/scenarios/tool/ErrorListener.java
+++ src/main/java/org/fulib/scenarios/tool/ErrorListener.java
@@ -1,10 +1,12 @@
 package org.fulib.scenarios.tool;
 
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 
 import java.io.PrintWriter;
+import java.util.BitSet;
+import java.util.stream.Collectors;
 
 public class ErrorListener extends BaseErrorListener
 {
@@ -20,6 +22,27 @@ public class ErrorListener extends BaseErrorListener
       String msg, RecognitionException e)
    {
       final String sourceName = recognizer.getInputStream().getSourceName();
-      this.out.println(sourceName + ":" + line + ":" + charPositionInLine + ": syntax: " + msg);
+      this.report(sourceName, line, charPositionInLine, "syntax", msg);
+   }
+
+   @Override
+   public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
+      BitSet ambigAlts, ATNConfigSet configs)
+   {
+      final String sourceName = recognizer.getInputStream().getSourceName();
+      final Token startToken = recognizer.getInputStream().get(startIndex);
+      final int line = startToken.getLine();
+      final int column = startToken.getCharPositionInLine();
+
+      final String[] ruleNames = recognizer.getRuleNames();
+      final String altString = ambigAlts.stream().mapToObj(i -> ruleNames[i]).sorted()
+                                        .collect(Collectors.joining(", "));
+
+      this.report(sourceName, line, column, "warning", "parser ambiguity with alternatives: " + altString);
+   }
+
+   private void report(String file, int line, int column, String type, String msg)
+   {
+      this.out.println(file + ":" + line + ":" + column + ": " + type + ": " + msg);
    }
 }
