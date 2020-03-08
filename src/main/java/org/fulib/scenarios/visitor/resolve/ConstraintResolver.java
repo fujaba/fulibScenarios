b584d017934942ffc9f6bf20c45e71ccaacdcddd
Adrian Kunz <clashsoft@hotmail.com>
2020-03-08 20:04:24 +0100

    Implement ConstraintResolver

--- src/main/java/org/fulib/scenarios/visitor/resolve/ConstraintResolver.java
+++ src/main/java/org/fulib/scenarios/visitor/resolve/ConstraintResolver.java
@@ -1,10 +1,37 @@
 package org.fulib.scenarios.visitor.resolve;
 
-import org.fulib.scenarios.ast.pattern.Constraint;
+import org.fulib.scenarios.ast.pattern.*;
 import org.fulib.scenarios.ast.scope.Scope;
 
 public enum ConstraintResolver implements Constraint.Visitor<Scope, Constraint>
 {
    INSTANCE;
-   // TODO
+
+   @Override
+   public Constraint visit(LinkConstraint linkConstraint, Scope par)
+   {
+      // TODO error
+      linkConstraint.setTarget(linkConstraint.getTarget().accept(NameResolver.INSTANCE, par));
+      return null;
+   }
+
+   @Override
+   public Constraint visit(AttributeEqualityConstraint attributeEqualityConstraint, Scope par)
+   {
+      attributeEqualityConstraint.setExpr(attributeEqualityConstraint.getExpr().accept(ExprResolver.INSTANCE, par));
+      return attributeEqualityConstraint;
+   }
+
+   @Override
+   public Constraint visit(AttributePredicateConstraint attributePredicateConstraint, Scope par)
+   {
+      return attributePredicateConstraint;
+   }
+
+   @Override
+   public Constraint visit(AttributeConditionalConstraint attributeConditionalConstraint, Scope par)
+   {
+      attributeConditionalConstraint.setRhs(attributeConditionalConstraint.getRhs().accept(ExprResolver.INSTANCE, par));
+      return attributeConditionalConstraint;
+   }
 }
